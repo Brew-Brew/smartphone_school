ca49ef27453e32439e52484b7ab04818ef00ead9
LEE SEUNG KYU <zx6658@naver.com>
2017-09-02 04:10:17 +0000

    another commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -104,11 +104,10 @@ quiz_list2=[
   end
   
 quiz_list3=[
-  [self.id,(self.id - 1)*6 + 3,'카메라 문제1','','O'],
-  [self.id,(self.id - 1)*6 + 3,'카메라 문제2','문제내용2','X'],
-  [self.id,(self.id - 1)*6 + 3,'카메라 문제3','문제내용3','O'],
-  [self.id,(self.id - 1)*6 + 3,'카메라 문제4','문제내용4','X'],
-  [self.id,(self.id - 1)*6 + 3,'카메라 문제5','문제내용5','O']
+  [self.id,(self.id - 1)*6 + 3,'카메라 문제1','사진은 갤러리 라는 앱에서 볼수있다.','O'],
+  [self.id,(self.id - 1)*6 + 3,'카메라 문제2','카카오톡 앱에서 저장한 사진은 KaKaoTalk 이라는 폴더에 따로 저장된다.','O'],
+  [self.id,(self.id - 1)*6 + 3,'카메라 문제3','화면에 손가락을 화면에 대고 벌리면 다른 사진을 확인 할 수 있다.','X'],
+  [self.id,(self.id - 1)*6 + 3,'카메라 문제4','공유 버튼을 통해 메시지로 사진을 전송할 수 있다.']
   ]
  
   quiz_list3.each do |user_id,subject_id,title,content,answer|
@@ -117,22 +116,18 @@ quiz_list3=[
 
 
 quiz_list4=[
-  [self.id,(self.id - 1)*6 + 4,'메뉴-4 문제1','문제내용1','O'],
-  [self.id,(self.id - 1)*6 + 4,'메뉴-4 문제2','문제내용2','X'],
-  [self.id,(self.id - 1)*6 + 4,'메뉴-4 문제3','문제내용3','O'],
-  [self.id,(self.id - 1)*6 + 4,'메뉴-4 문제4','문제내용4','X'],
-  [self.id,(self.id - 1)*6 + 4,'메뉴-4 문제5','문제내용5','O']
+  [self.id,(self.id - 1)*6 + 4,'환경설정 문제1','환경설정-디스플레이에서 배경화면을 바꿀수 있다.','O'],
+  [self.id,(self.id - 1)*6 + 4,'환경설정 문제2','홈화면과 잠금화면을 다르게 설정할 수 있다.','O'],
+  [self.id,(self.id - 1)*6 + 4,'환경설정 문제3','와이파이 신호 오른쪽에 자물쇠 모양이 있으면 비밀번호를 입력하고 연결 해야 한다.','O']
   ]
  
   quiz_list4.each do |user_id,subject_id,title,content,answer|
    Quiz.create(user_id: user_id,subject_id: subject_id,title: title,content: content,answer: answer)
   end
 quiz_list5=[
-  [self.id,(self.id - 1)*6 + 5,'메뉴-5 문제1','문제내용1','O'],
-  [self.id,(self.id - 1)*6 + 5,'메뉴-5 문제2','문제내용2','X'],
-  [self.id,(self.id - 1)*6 + 5,'메뉴-5 문제3','문제내용3','O'],
-  [self.id,(self.id - 1)*6 + 5,'메뉴-5 문제4','문제내용4','X'],
-  [self.id,(self.id - 1)*6 + 5,'메뉴-5 문제5','문제내용5','O']
+  [self.id,(self.id - 1)*6 + 5,'마켓/어플 문제1','어플을 다운받는 앱의 이름은 PLAY 마켓이다.','X'],
+  [self.id,(self.id - 1)*6 + 5,'마켓/어플 문제2','스토어에서 어플을 다운받고, 다운이 다되면 바로 열기를 할 수 있다.','O'],
+  [self.id,(self.id - 1)*6 + 5,'마켓/어플 문제3','환경설정-애플리케이션에 들어가면 어플을 삭제 할 수 있다.','O']
   ]
  
   quiz_list5.each do |user_id,subject_id,title,content,answer|
@@ -140,11 +135,11 @@ quiz_list5=[
   end
   
   quiz_list6=[
-  [self.id,(self.id - 1)*6 + 6,'메뉴-6 문제1','문제내용1','O'],
-  [self.id,(self.id - 1)*6 + 6,'메뉴-6 문제2','문제내용2','X'],
-  [self.id,(self.id - 1)*6 + 6,'메뉴-6 문제3','문제내용3','O'],
-  [self.id,(self.id - 1)*6 + 6,'메뉴-6 문제4','문제내용4','X'],
-  [self.id,(self.id - 1)*6 + 6,'메뉴-6 문제5','문제내용5','O']
+  [self.id,(self.id - 1)*6 + 6,'SNS 문제1','카카오톡 메시지 입력창의 오른쪽 얼굴 모양을 누르면 이모티콘을 보낼 수 있다.','O'],
+  [self.id,(self.id - 1)*6 + 6,'SNS 문제2','사진이나 동영상을 보내려면 대화창의 - 버튼을 누르면 된다.','X'],
+  [self.id,(self.id - 1)*6 + 6,'SNS 문제3','카카오톡에는 통화할수 있는 인터넷 전화인 보이스톡이라는 서비스가 있다.','O'],
+  [self.id,(self.id - 1)*6 + 6,'SNS 문제4','네이버밴드에서 연락처로 초대에서 초대하기를 하면 문자로 자동으로 초대메세지가 입력된다.','O'],
+  [self.id,(self.id - 1)*6 + 6,'SNS 문제5','네이버 밴드에서는 문자말고 다른방법으로도 초대가 가능하다.','O']
   ]
  
   quiz_list6.each do |user_id,subject_id,title,content,answer|
